d0e8835d1f43da0d49c356842cbb6d80084ef488
Admin <Admin@DESKTOP-EFDTCLP>
2018-08-21 10:20:48 -0500

    Modificación WS que busca seguimientos para regresar el listado JSON de documentos relacionados

--- TurboERP_backend/src/main/java/com/turbomaquinas/DAO/calidad/SeguimientoInsatisfaccionVistaRM.java
+++ TurboERP_backend/src/main/java/com/turbomaquinas/DAO/calidad/SeguimientoInsatisfaccionVistaRM.java
@@ -1,11 +1,16 @@
 package com.turbomaquinas.DAO.calidad;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.jdbc.core.RowMapper;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.turbomaquinas.POJO.calidad.SeguimientoInsatisfaccionVista;
+import com.turbomaquinas.POJO.calidad.SeguimientoInsatisfaccionVista.Alfresco_ids;
 
 public class SeguimientoInsatisfaccionVistaRM implements RowMapper<SeguimientoInsatisfaccionVista> {
 
@@ -23,7 +28,17 @@ public class SeguimientoInsatisfaccionVistaRM implements RowMapper<SeguimientoIn
 		seguimiento.setModificado(rs.getDate("modificado"));
 		seguimiento.setModificado_por(rs.getInt("modificado_por"));
 		seguimiento.setInsatisfacciones_clientes_id(rs.getInt("INSATISFACCIONES_CLIENTES_id"));
-		seguimiento.setAlfresco_ids(rs.getString("alfresco_ids"));
+		
+		//seguimiento.setAlfresco_ids(rs.getString("alfresco_ids"));
+		
+		String json=rs.getString("alfresco_ids");
+		ObjectMapper mapper = new ObjectMapper();
+		List<Alfresco_ids> alfresco=null;
+		try {
+			alfresco = mapper.readValue(json, new TypeReference<List<Alfresco_ids>>(){});
+		} catch (IOException e) {System.out.println(e);}
+		seguimiento.setAlfresco_ids(alfresco);
+		
 		seguimiento.setCantidad_Documentos(rs.getInt("cantidad_Documentos"));
 		seguimiento.setFolio_insatisfaccion(rs.getInt("folio_insatisfaccion"));
 		seguimiento.setORDENES_id(rs.getInt("ORDENES_id"));
